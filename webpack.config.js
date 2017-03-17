508f675ccef894ff07440bbd99d44e3eae6b7b82
yinxueyuan <wb-yxy230225@alibaba-inc.com>
2017-03-17 09:46:42 +0800

    feat: server

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: {
+		index: path.resolve(__dirname, './src/pages/home')
+	},
+	resolve: {
+		extensions: ['', '.js', 'jsx']
+	},
+	module: {
+		loaders: [
+      {
+        test: /\.js[x]?$/,
+        loader: 'babel-loader',
+        include: path.resolve(__dirname, './src'),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss|.css$/,
+        loaders: ['style', 'css', 'sass']
+      }
+		]
+	},
+  output: {
+    publicPath: '/static',
+    path: path.resolve(__dirname, './static'),
+    filename: 'js/[name].js'
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({}),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+console.log(123)
