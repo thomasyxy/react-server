ea4a5072d9a4d66273aee1cc1de2db04e7a4923d
yinxueyuan <yinxueyuan@leoao.com>
2017-03-28 17:18:49 +0800

    fix: koa-webpack

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-"use strict";
-const koa = require('koa');
-const convert = require('koa-convert');
-const app = new koa();
-const Jade = require('koa-jade');
+"use strict"
+const koa = require('koa')
+const convert = require('koa-convert')
+const app = new koa()
+const Jade = require('koa-jade')
 const staticCache = require('koa-static-cache');
 const webpack = require('webpack');
-const ora = require('ora');
-const routes = require('./config/routes');
-const webpackConfig = require('./webpack.config');
+const ora = require('ora')
+const routes = require('./config/routes')
+const webpackConfig = require('./webpack.config')
 
-const port = 5000;
+const port = 5000
 
 //静态资源文件
 app.use(convert(staticCache('./static', {
   maxAge: 0
-})));
+})))
 
 const jade = new Jade({
   viewPath: __dirname + "/views",
@@ -25,47 +25,55 @@ const jade = new Jade({
     staticPath: '/static'
   },
   app: app
-});
+})
 
 //路由
-app.use(convert(routes()));
+app.use(convert(routes()))
 
 app.use(convert(function*(next) {
   yield next
-  if (404 !== this.status) return;
-  this.status = 404;
+  if (404 !== this.status) return
+  this.status = 404
   this.render('404', {
     msg: 'Not Found'
   })
-}));
+}))
 
-const compiler = webpack(webpackConfig)
+const spinner = ora('building...')
+spinner.start()
 
-const devMiddleware = require('webpack-dev-middleware')(compiler, {
-  publicPath: webpackConfig.output.publicPath,
-  stats: {
-    colors: true,
-    chunks: false
-  }
-})
-
-const hotMiddleware = require('webpack-hot-middleware')(compiler)
-// force page reload when html-webpack-plugin template changes
-compiler.plugin('compilation', function (compilation) {
-  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-    hotMiddleware.publish({ action: 'reload' })
-    cb()
+function webpackBuild() {
+  const compiler = webpack(webpackConfig, () => {
+    spinner.stop()
   })
-})
+  const devMiddleware = require("koa-webpack-dev-middleware")(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    static: {
+      colors: true,
+      chunks: false
+    }
+  })
 
-app.use(convert(devMiddleware))
+  const hotMiddleware = require('koa-webpack-hot-middleware')(compiler)
 
-app.use(convert(hotMiddleware))
+  compiler.plugin('compilation', function (compilation) {
+    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+      hotMiddleware.publish({ action: 'reload' })
+      cb()
+    })
+  })
+
+  app.use(convert(devMiddleware))
+
+  app.use(convert(hotMiddleware))
+}
+
+webpackBuild()
 
 app.listen(port, (err) => {
 	if(err){
-		console.log(err);
-		return false;
+		console.log(err)
+		return false
 	}
-	console.log(`> listen at: http://127.0.0.1:${port}`);
-});
+	console.log(`> listen at: http://127.0.0.1:${port}`)
+})
