31f11a9a3a0ecdfbb397a91c8ffca2b64923c76b
yinxueyuan <838476901@qq.com>
2017-04-22 17:59:37 +0800

    feat: react server create

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,31 @@
 "use strict";
 const router = require('koa-router')();
+const React = require('react');
+import ReactDOM from 'react-dom/server';
+import App from '../src/pages/home/mods/app.jsx';
 
 
 module.exports = function(app) {
+
+  router.get('/', function *(next) {
+    this.render('index',{
+      title: 'hello'
+    })
+
+    yield next
+  });
+
+  router.get('/react', function *(next) {
+    let Component = React.createFactory(App)
+
+    console.log(Component)
+
+    this.render('index', {
+      react: ReactDOM.renderToString(Component({name: "John"}))
+    })
+
+    yield next
+  })
 
   router.get('/', function *(next) {
     this.render('index',{
